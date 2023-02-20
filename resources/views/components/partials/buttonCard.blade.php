3df8d1f81580501ec474ae7734c7b1939f051dfc
Elvira Memeti <em29233@seeu.edu.mk>
2023-02-20 13:22:12 +0100

    Button update

--- resources/views/components/partials/buttonCard.blade.php
+++ resources/views/components/partials/buttonCard.blade.php
@@ -1,6 +1,6 @@
 
 <div>
   <button type="button" style="font-size: 12px; text-align:center; background: #152238;  
-   padding:10px; border-radius:12px; border-color: #152238;  margin-left:86%; margin-top: 10px; " >
+   padding:10px; border-radius:12px; border-color: #152238;   margin-top: 10px; " >
   <a href="/applicants/studentRegister" style="color:white">  Add Student </a> </button> 
   </div>
